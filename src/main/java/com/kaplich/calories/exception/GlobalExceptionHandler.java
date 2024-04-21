c828cfbc0814a514a140371eb8fb5b41c6f1d034
Валерия <lerakaplich@gmail.com>
2024-04-22 01:40:11 +0300

    fixup! fix! fixup!

--- src/main/java/com/kaplich/calories/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kaplich/calories/exception/GlobalExceptionHandler.java
@@ -1,22 +1,12 @@
 package com.kaplich.calories.exception;
 
-import lombok.NonNull;
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.MissingRequestValueException;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 @Validated
